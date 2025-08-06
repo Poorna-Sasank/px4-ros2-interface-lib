c2197f205230f9dbc36588d8ec46761002096265
poorna-sasank-sesetti <poorna.sesetti03@gmail.com>
2025-08-06 10:48:12 +0530

    nothing

--- examples/cpp/modes/manual/include/mode.hpp
+++ examples/cpp/modes/manual/include/mode.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <px4_ros2/components/mode.hpp>
 #include <px4_ros2/control/setpoint_types/experimental/attitude.hpp>
 #include <px4_ros2/control/peripheral_actuators.hpp>
@@ -7,52 +8,24 @@
 #include <Eigen/Eigen>
 #include <rclcpp/rclcpp.hpp>
 
-using namespace std::chrono_literals;
-
-static const std::string kName = "NoseDiveControl";
-
 class FlightModeTest : public px4_ros2::ModeBase
 {
 public:
-  explicit FlightModeTest(rclcpp::Node & node)
-  : ModeBase(node, kName)
-  , _attitude_setpoint(std::make_shared<px4_ros2::AttitudeSetpointType>(*this))
-  , _peripheral_actuator_controls(std::make_shared<px4_ros2::PeripheralActuatorControls>(*this))
-  , _vehicle_local_position(std::make_shared<px4_ros2::OdometryLocalPosition>(*this))
-  , _nose_dive_pitch(-0.5410f) // -31 degrees in radians (nose down)
-  , _nose_dive_thrust(0.70f)   // Thrust for rapid descent with attitude control
-  , _yaw(1.574f)               // Fixed yaw (90 degrees)
-  {
-  }
+    explicit FlightModeTest(rclcpp::Node& node);
 
-  void onActivate() override 
-  {
-  }
-
-  void onDeactivate() override 
-  {
-  }
-
-  void updateSetpoint(float dt_s) override
-  {
-    // Nose dive: fixed pitch, zero roll, low thrust
-    Eigen::Vector2f pitch_roll(0.0f, _nose_dive_pitch); // Roll = 0, Pitch = -31 degrees
-    float thrust = _nose_dive_thrust;
-
-    // Convert pitch, roll, yaw to quaternion
-    const Eigen::Quaternionf qd = px4_ros2::eulerRpyToQuaternion(pitch_roll.x(), pitch_roll.y(), _yaw);
-    const Eigen::Vector3f thrust_sp{0.0f, 0.0f, -thrust};
-
-    _attitude_setpoint->update(qd, thrust_sp, 0.0f);
-    _peripheral_actuator_controls->set(0.0f);
-  }
+    void onActivate() override;
+    void onDeactivate() override;
+    void updateSetpoint(float dt_s) override;
 
 private:
-  std::shared_ptr<px4_ros2::AttitudeSetpointType> _attitude_setpoint;
-  std::shared_ptr<px4_ros2::PeripheralActuatorControls> _peripheral_actuator_controls;
-  std::shared_ptr<px4_ros2::OdometryLocalPosition> _vehicle_local_position;
+    void loadParameters();
 
-  float _nose_dive_pitch; // Pitch angle for nose dive (radians)
-  float _nose_dive_thrust; // Thrust for nose dive
-  float _yaw; // Fixed yaw angle
+    rclcpp::Node& _node;
+    std::shared_ptr<px4_ros2::AttitudeSetpointType> _attitude_setpoint;
+    std::shared_ptr<px4_ros2::PeripheralActuatorControls> _peripheral_actuator_controls;
+    std::shared_ptr<px4_ros2::OdometryLocalPosition> _vehicle_local_position;
+
+    float _param_nose_dive_pitch = {};
+    float _param_nose_dive_thrust = {};
+    float _param_yaw = {};
 };
--- examples/cpp/modes/manual/src/main.cpp
+++ examples/cpp/modes/manual/src/main.cpp
@@ -1,22 +1,58 @@
-/****************************************************************************
- * Copyright (c) 2023 PX4 Development Team.
- * SPDX-License-Identifier: BSD-3-Clause
- ****************************************************************************/
+#include "mode.hpp"
 
-#include "rclcpp/rclcpp.hpp"
-
-#include <mode.hpp>
 #include <px4_ros2/components/node_with_mode.hpp>
+#include <Eigen/Core>
 
-using MyNodeWithMode = px4_ros2::NodeWithMode<FlightModeTest>;
-
-static const std::string kNodeName = "example_mode_manual";
+static const std::string kModeName = "NoseDiveControl";
 static const bool kEnableDebugOutput = true;
 
-int main(int argc, char * argv[])
+using namespace std::chrono_literals;
+
+FlightModeTest::FlightModeTest(rclcpp::Node& node)
+    : ModeBase(node, kModeName)
+    , _node(node)
+    , _attitude_setpoint(std::make_shared<px4_ros2::AttitudeSetpointType>(*this))
+    , _peripheral_actuator_controls(std::make_shared<px4_ros2::PeripheralActuatorControls>(*this))
+    , _vehicle_local_position(std::make_shared<px4_ros2::OdometryLocalPosition>(*this))
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MyNodeWithMode>(kNodeName, kEnableDebugOutput));
-  rclcpp::shutdown();
-  return 0;
+    loadParameters();
+}
+
+void FlightModeTest::loadParameters()
+{
+    _node.declare_parameter<float>("nose_dive_pitch", 0.0);
+    _node.declare_parameter<float>("nose_dive_thrust", 0.744);
+    _node.declare_parameter<float>("yaw", 1.57);
+
+    _node.get_parameter("nose_dive_pitch", _param_nose_dive_pitch);
+    _node.get_parameter("nose_dive_thrust", _param_nose_dive_thrust);
+    _node.get_parameter("yaw", _param_yaw);
+}
+
+void FlightModeTest::onActivate()
+{
+}
+
+void FlightModeTest::onDeactivate()
+{
+}
+
+void FlightModeTest::updateSetpoint(float dt_s)
+{
+    Eigen::Vector2f pitch_roll(0.0f, _param_nose_dive_pitch);
+    float thrust = _param_nose_dive_thrust;
+
+    const Eigen::Quaternionf qd = px4_ros2::eulerRpyToQuaternion(pitch_roll.x(), pitch_roll.y(), _param_yaw);
+    const Eigen::Vector3f thrust_sp{0.0f, 0.0f, -thrust};
+
+    _attitude_setpoint->update(qd, thrust_sp, 0.0f);
+    _peripheral_actuator_controls->set(0.0f);
+}
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<px4_ros2::NodeWithMode<FlightModeTest>>(kModeName, kEnableDebugOutput));
+    rclcpp::shutdown();
+    return 0;
 }
